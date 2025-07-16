29b17b7fc6a5ca5e6d06df2ae6e0651ad5d85c0a
Aneesh <aneeshy1508cse.com>
2025-07-16 11:00:48 +0530

    first commit

---
+++ client/src/pages/BuyCredit.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const BuyCredit = () => {
+  return (
+    <div>BuyCredit</div>
+  )
+}
+ export default BuyCredit
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../components/Header'
+import Steps from '../components/Steps'
+import Description from '../components/Description'
+
+export const Home = () => {
+  return (
+    <div>
+      <Header/>
+      <Steps/>
+      <Description/>
+    </div>
+  )
+}
+export default Home
