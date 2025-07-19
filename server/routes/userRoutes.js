6534160e83c70bc18487e64376359714e26eadd6
Aneesh <aneeshy1508cse.com>
2025-07-19 19:04:53 +0530

    backend

---
+++ server/routes/userRoutes.js
@@ -0,0 +1,10 @@
+import {registerUser, userCredits} from "../controllers/userControllers.js";
+import {loginUser} from "../controllers/userControllers.js";
+import express from 'express';
+import userAuth from "../middlewares/auth.js";
+
+const userRouter = express.Router()
+userRouter.post('/register', registerUser)
+userRouter.post('/login', loginUser)
+userRouter.get('/credits',userAuth, userCredits)
+export default userRouter
